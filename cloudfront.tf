a36f3c1aa9456b159e278e58e40b58f3a32f41da
Jonathan Gaul <jon@gaul.tech>
2025-06-10 21:15:23 +0000

    Add custom origin config to cloudfront distribution for API

--- cloudfront.tf
+++ cloudfront.tf
@@ -22,6 +22,12 @@ resource "aws_cloudfront_distribution" "cdn" {
   origin {
     domain_name = module.api.invoke_url
     origin_id   = "API-Gateway"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = "TLSv1.2"
+    }
   }
 
   default_cache_behavior {
