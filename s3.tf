638aebcdb3b7377f463686fc06ebaf37d397e0d1
Jonathan Gaul <jon@gaul.tech>
2025-06-10 23:03:04 +0000

    Update policy for frontend bucket

--- s3.tf
+++ s3.tf
@@ -9,3 +9,20 @@ resource "aws_s3_bucket" "source_chunks" {
 resource "aws_s3_bucket" "frontend" {
   bucket = "${var.project}-${var.environment}-frontend"
 }
+
+resource "aws_s3_bucket_policy" "cdn_policy" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    Id      = "PolicyForCloudFrontPrivateContent",
+    Version = "2008-10-17",
+    Statement = [
+      {
+        Sid       = "1",
+        Effect    = "Allow",
+        Principal = { AWS = aws_cloudfront_origin_access_identity.oai.iam_arn },
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+}
