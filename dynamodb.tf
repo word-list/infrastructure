c6387b10ecfa849402e884db561dd8a2c3eb6112
Jonathan Gaul <jon@gaul.tech>
2025-06-21 20:20:53 +0000

    Use sha256 not filesha256

--- dynamodb.tf
+++ dynamodb.tf
@@ -104,7 +104,7 @@ resource "local_file" "word_attributes" {
 
 resource "null_resource" "load_word_attributes" {
   triggers = {
-    file_sha = filesha256(local_file.word_attributes.content)
+    file_sha = sha256(local_file.word_attributes.content)
   }
 
   provisioner "local-exec" {
