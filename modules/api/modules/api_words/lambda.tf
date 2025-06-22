97e067b8d7b778a19faf49b036ff7635f06d74a7
Jonathan Gaul <jon@gaul.tech>
2025-06-22 16:20:07 +0000

    Use dotnet runtime for api-words

--- modules/api/modules/api_words/lambda.tf
+++ modules/api/modules/api_words/lambda.tf
@@ -1,11 +1,12 @@
 resource "aws_lambda_function" "api_words" {
   function_name    = "${var.project}-${var.environment}-api-words"
-  handler          = "bootstrap"
-  runtime          = "provided.al2023"
+  handler          = "WordList.Api.Words"
+  runtime          = "dotnet8"
   role             = aws_iam_role.api_words.arn
+  architectures    = ["arm64"]
   filename         = "placeholder.zip"
   source_code_hash = filebase64sha256("placeholder.zip")
-  architectures    = ["arm64"]
+  timeout          = 60
   environment {
     variables = {
       DB_CONNECTION_STRING = var.db_connection_string
